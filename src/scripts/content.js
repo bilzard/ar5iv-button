ba519917c2cfd8773dcf2ef62e65c94243bb13d4
bilzard <nakamine11@gmail.com>
2022-11-08 18:01:19 +0900

    add v1 resource

---
+++ src/scripts/content.js
@@ -0,0 +1,10 @@
+const ar5ivUrl = document.URL.replace("arxiv.org", "ar5iv.org");
+
+const button = document.createElement("button");
+button.id = "ar5iv_button";
+button.innerHTML = "ar<font color='red'>5</font>iv";
+button.onclick = () => {
+    window.open(ar5ivUrl, "_blank").focus();
+};
+const headerElement = document.querySelector("h1.title");
+headerElement.appendChild(button);
